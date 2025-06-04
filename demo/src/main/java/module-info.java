fc08508220aab3d0c92fc3e5d9a7c8432444a89e
sharondarmap <19623106@std.stei.itb.ac.id>
2025-06-04 21:02:16 +0700

    feat: define the properties of modules

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,7 +1,17 @@
 module com.pekaboo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
+    // Open FXML-based packages for controller injection
     opens com.pekaboo to javafx.fxml;
-    exports com.pekaboo;
+    opens com.pekaboo.util to javafx.fxml;
+
+    // Controllers for each feature (used by FXMLLoader)
+    opens com.pekaboo.features.jadwal to javafx.fxml;
+    opens com.pekaboo.features.pembelian to javafx.fxml;
+    opens com.pekaboo.features.reservasi to javafx.fxml;
+
+    // Open entities package for TableView & property bindings
+    opens com.pekaboo.entities to javafx.base;
 }
